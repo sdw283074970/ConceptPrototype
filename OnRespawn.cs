4d36bd2fda10791619eeb463a7d974c007c704b3
sdw283074970 <downwes@gmail.com>
2017-07-20 01:28:19 -0400

    Update OnRespawn.cs

--- OnRespawn.cs
+++ OnRespawn.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-//此脚本挂载在PlayerPerfab上
+//此脚本挂载在CamRig上
 namespace Player
 {
 	public class OnRespawn : MonoBehaviour {
@@ -14,10 +14,9 @@ namespace Player
 
 			GameObject.Find("ItemChecker").GetComponent<ItemsAround>().enabled = true;
 			GameObject.Find("CoverChecker").GetComponent<CoverTargetLogic>().enabled = true;
-			GameObject.Find("Player Y_Bot").GetComponent<PlayerAtts>().enabled = true;
-			GameObject.Find("Player Y_Bot").GetComponent<SetupAndUserInput>().enabled = true;
-			GameObject.Find("Player Y_Bot").GetComponent<Animator>().enabled = true;
-			GameObject.Find("CamRig").GetComponent<NewRespawn>().enabled = true;
+			GameObject.FindWithTag("Player").GetComponent<PlayerAtts>().enabled = true;
+			GameObject.FindWithTag("Player").GetComponent<SetupAndUserInput>().enabled = true;
+			GameObject.FindWithTag("Player").GetComponent<Animator>().enabled = true;
 		}
 
 		// Update is called once per frame
